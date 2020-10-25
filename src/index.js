af2f1042d4f68402fb71be5e65bfd8946df11af8
VadymKas <v.kaschin@gmail.com>
2020-10-25 15:36:36 +0200

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let result = bracketsConfig.flat().join('');
+    let stack = [];
+
+    for(let bracket of str) {
+        let bracketsIndex = result.indexOf(bracket);
+
+
+        if (stack.length > 0 && result[bracketsIndex] === result[stack[stack.length - 1]]) {
+            stack.pop();
+            continue;
+        }
+
+        if(bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1)
+        } else {
+            if(stack.pop() !== bracketsIndex) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0
+};
